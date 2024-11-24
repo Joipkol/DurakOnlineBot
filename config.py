2bfb8110ee8aa62c959440e2b857c444de79b5f7
Joipkol <maksimpomorcev01@gmail.com>
2024-11-24 23:22:22 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 SIGN_KEY = 'oc3q7ingf978mx457fgk4587fg847'
 RUCAPTCHA_KEY = ''
-NAME = ''
-USER_ID = 0
-TOKEN = ''
+NAME = 'Domxod))))'
+USER_ID = Hu07sU0e
+TOKEN = 'auth{"token":"$2a$06$DC5ltIId1D7xGdXgMTMPtO"}'
 HUMAN_CAPTCHA_SOLVE = True
-level = 'INFO'
+level = '0'
